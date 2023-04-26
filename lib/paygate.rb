c1bcf15e9d48646a4f75665ba7500bd30d086896
johnnyshields <johnny.shields@gmail.com>
2023-04-26 22:22:05 +0900

    Re-add Rails Engine (was dropped in version 0.2.0) Rename Paygate::ActionView::FormHelper to Paygate::Rails::FormHelper.

--- lib/paygate.rb
+++ lib/paygate.rb
@@ -9,19 +9,20 @@ require 'paygate/member'
 require 'paygate/response'
 require 'paygate/transaction'
 require 'paygate/profile'
-require 'paygate/action_view/form_helper' if defined?(ActionView)
+
+if defined?(Rails)
+  require 'paygate/rails/engine'
+  require 'paygate/rails/form_helper'
+end
 
 module Paygate
   extend self
 
   CONFIG = YAML.safe_load(File.read(File.expand_path('../data/config.yml', __dir__)),
                           permitted_classes: [Symbol]).freeze
-  LOCALES_MAP = CONFIG[:locales].freeze
-  LOCALES_MAP.each { |k, v| k.freeze; v.freeze } # rubocop:disable Style/Semicolon
-  INTL_BRANDS_MAP = CONFIG[:intl_brands].freeze
-  INTL_BRANDS_MAP.each_value(&:freeze)
-  KOREA_BIN_NUMBERS = CONFIG[:korea_bin_numbers].freeze
-  KOREA_BIN_NUMBERS.each(&:freeze)
+  (LOCALES_MAP = CONFIG[:locales].freeze).each { |k, v| k.freeze; v.freeze } # rubocop:disable Style/Semicolon
+  (INTL_BRANDS_MAP = CONFIG[:intl_brands].freeze).each_value(&:freeze)
+  (KOREA_BIN_NUMBERS = CONFIG[:korea_bin_numbers].freeze).each(&:freeze)
   DEFAULT_CURRENCY = 'WON'
   DEFAULT_LOCALE = 'US'
 
